586aa290a7f1c40fd4220508a484b8368adaeb46
Rafael-Cerqueira <rafael.scerqueira@hotmail.com>
2022-11-24 06:50:30 -0300

    dev estrutura da pokedex em css e html

---
+++ Desafio JS/arraysLacos.js
@@ -0,0 +1,29 @@
+//  const notas = [];
+
+//  notas.push(6)
+//  notas.push(9)
+//  notas.push(4.5)
+
+// let soma = 0;
+
+// for (let i = 0; i < notas.length; i++){
+//     const nota = notas[i];
+//     soma = soma + nota;
+// }
+// media = soma/notas.length;
+// console.log('A média das notas é:',media);
+
+// const nome = 'Rafael Cerqueira'
+
+// for (let i = 0; i < nome.length; i++){
+//     console.log(nome[i]);
+// }
+
+let numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+for (i = 0; i <= numeros.length; i++){
+    const numero = numeros[i]; 
+    if(numero % 2 === 0){
+        console.log(numero); 
+    } 
+}

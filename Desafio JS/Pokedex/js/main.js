cbcc8945f135a620c71c6e25ed768811d1bcde0a
Rafael-Cerqueira <rafael.scerqueira@hotmail.com>
2022-11-29 16:10:26 -0300

    Primeira Versão do Projeto Pronto

--- Desafio JS/Pokedex/js/main.js
+++ Desafio JS/Pokedex/js/main.js
@@ -1,5 +1,46 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const maxRecord = 151
+const limit = 5
+let offset = 0;
 
-fetch(url)
+
+
+function loadPokemonItens(offset, limit){    
+        pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => 
+        `
+            <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" 
+                alt="${pokemon.name}">
+            </div>
+        </li>`).join('')
+        pokemonList.innerHTML += newHtml
+    
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecord){
+        const newLimit = maxRecord - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    }else {
+        loadPokemonItens(offset, limit)
+    }
+    
+})
